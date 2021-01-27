4f21ddf238320e79069e25d95f71523f2248cd06
Pragyesh <73213155+Pragyesh-Kumar@users.noreply.github.com>
2021-01-27 18:32:34 +0530

    Add files via upload

---
+++ CombinationNCRRecursion.cpp
@@ -0,0 +1,33 @@
+//Combination Formula
+#include <stdio.h>
+int fact(int n)
+{
+	if(n==0)
+		return 1;
+	return fact(n-1)*n;
+}
+
+//Combination using factorial
+int factNCR(int n,int r)
+{
+	int num,den;
+	num=fact(n);
+	den=fact(r)*fact(n-r);
+	return num/den;
+}
+
+//Combination using Recursion
+int recurNCR(int n,int r)
+{
+	if(n==r || r==0)
+		return 1;
+	return recurNCR(n-1,r-1) + recurNCR(n-1,r);
+}
+
+int main()
+{
+	printf("Combination using factorial : %d \n", factNCR(5,3));
+
+	printf("Combination using Recursion : %d \n", recurNCR(5,3));
+	return 0;
+}
