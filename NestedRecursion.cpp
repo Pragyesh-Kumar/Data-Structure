be978b5389c9b4e0da242ea352428beb7b462678
Pragyesh <73213155+Pragyesh-Kumar@users.noreply.github.com>
2021-01-27 17:13:24 +0530

    Add files via upload

---
+++ NestedRecursion.cpp
@@ -0,0 +1,19 @@
+//Nested Recursion
+#include <stdio.h>
+int fun(int n)
+{
+	if(n>100)
+	{
+		printf("%d\n",n);
+		return n-10;
+	}
+
+	return fun(fun(n+11));
+}
+int main()
+{
+	int r;
+	r=fun(95);
+	printf("%d\n",r);
+	return 0;
+}
